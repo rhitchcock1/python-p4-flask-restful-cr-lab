366fd14b538410073c316e9cf04c44059dae3ba3
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-04-06 17:46:39 -0400

    done

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound
 
 from models import db, Plant
 
@@ -16,12 +17,41 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+    response = make_response( {"error" : "No info here gal!"}, 404 )
+    return response 
+
+@app.route("/")
+def index():
+    return "<h1>Plant Api</h1>"
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_dict_list = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(plant_dict_list, 200)
+        return response
+    def post(self):
+        new_plant = Plant(
+            name = request.form["name"],
+            image = request.form["image"],
+            price = request.form["price"],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response_dict = new_plant.to_dict()
+        response = make_response(response_dict,201)
+        return response
+    
+api.add_resource(Plants, "/plants")    
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        response_dict = Plant.query.filter_by(id = id).first().to_dict()
+        response = make_response(response_dict, 200)
+        return response
+api.add_resource(PlantByID, "/plants/<int:id>")        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
